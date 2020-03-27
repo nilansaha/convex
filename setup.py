44dc284d3778bea840e5b585866cc3985f45c4cf
Nilan Saha <nilan.saha@alumni.ubc.ca>
2020-03-27 01:27:14 -0700

    Version 0.0.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('requirements.txt') as f:
 
 setup(
 	name='convex',
-	version='0.0.1',
+	version='0.0.2',
 	author='Nilan Saha',
 	description = 'A Light-weight Python NLP Library',
 	long_description = long_description,
